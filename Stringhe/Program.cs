5fcbeaae510ff0e22d7fa6bc27dd5e5b950dde9a
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-04-28 15:13:52 +0200

    Aggiungere i file di progetto.

---
+++ Stringhe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Corso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringa = "prova \" metto anche le virgolette\" ";
+            Console.WriteLine(stringa);
+            //\n vado a capo come in HTML
+            
+            //CONCATENAZIONE
+            string nome = "Angelo";
+            string cognome = "Dalo";
+            string stringa2 = "ciao sono " + nome  + " " + cognome;
+            //FORMATTAZIONE
+            Console.WriteLine("ciao sono {0} {1}", nome, cognome);
+            string stringa3 = string.Format("ciao sono {0} {1}", nome, cognome);
+            //INTERPOLLAZIONE
+            Console.WriteLine($"buongiorno sono {nome} {cognome}");
+
+            //METODI
+            string stringa4 = nome.ToUpper();
+            string stringa5 = nome.ToLower();
+            int stringa6 = nome.Length;
+            int stringa7 = nome.IndexOf("A");
+            Console.WriteLine(stringa6);
+            Console.WriteLine(stringa7);
+        }
+    }
+}
